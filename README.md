42f438ee060a76df725a13b2aa235838254e8590
Jed <jed-frey@users.noreply.github.com>
2016-12-13 15:19:41 -0500

    Nanos gigantum humeris insidentes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PyLTIGUI
+
 PyQT based GUI for Linear Time Invariant Systems
+
+Python's toolchest for LTI systems lags a bit behind Matlab in terms of polish.
+
+Us mechanical engineers don't like to mash keyboards we like pretty GUIs to mash instead.
---
+++ ltigui/firstorder.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from PyQt5.uic import loadUiType
+from PyQt5 import QtCore, QtWidgets
+import sys
+
+import numpy as np
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+
+Ui_MainWindow, QMainWindow = loadUiType('firstorder.ui')
+#from window2 import Ui_MainWindow
+
+from scipy import signal
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, ):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+        self.canvas = FigureCanvas(self.fig)
+        self.verticalLayout.addWidget(self.canvas)
+        
+        self.toolbar = NavigationToolbar(self.canvas, self.widget, coordinates=True)
+        self.verticalLayout.addWidget(self.toolbar)
+        
+        self.plotButton.clicked.connect(self.makeplot)
+        
+    def makeplot(self):
+         print("Hello World")
+         K = self.gain.value()
+         tau = self.tau.value()
+
+         sys = signal.TransferFunction([K],[tau,1])
+         T, yout = signal.step(sys)
+         self.ax.clear()
+         self.ax.plot(T,yout)
+         self.canvas.draw()
+        
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)  # A new instance of QApplication
+    main = MainWindow() 
+    main.show()
+    sys.exit(app.exec_())
